0a1e5497937f5e367ede49e2b1a7ffaa62f5c954
Blessed Dhewa <154527751+DELMAX29@users.noreply.github.com>
2024-08-11 23:10:47 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# K29-Md 
+<h1 align="center"> ☪️SUPER BOT☪️ <br></h1>
+<p align="center">
+<a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Fira+Code&weight=602&pause=1000&color=F70000&random=false&width=435&lines=SUPER-BOT+made+by+Jayden+Frank+;Follow+me+IG+%40confronter._" alt="Typing SVG" /></a>
+  
+  <img src="https://telegra.ph/file/0d4f06d19062a2ce8cafd.jpg" />
+</p>
+
+<p align="center">
+SUPER BOT Multi Device is a whatsapp bot created by <a href="https://github.com/Confronter" target="_blank">JAYDEN+&+FRANK</a> . Star the repo with courage 🌟.
+</p>
+
+
+
+## ✳️```SUPER Group & Channel ✳️```
+
+- [ GROUP ](https://chat.whatsapp.com/Cl7CwM1UC9YEOWEiCzLAfe)
+- [Channel](https://whatsapp.com/channel/0029Vag3MeuGJP8LZb1Okj39)
+
+## ♻️♻️MADE BY 𝙹𝙰𝚈𝙳𝙴𝙽 🪓𝙲𝙾𝙽𝙵𝚁𝙾𝙽𝚃𝙴𝚁
+
+# ✴️```Fork the repository📲```
+
+- FORK THE REPOSITORY [Here](https://github.com/jayden-official/SUPER-BOT/fork)
+
+## ☪️ `SUPER PAIRING CODE`
+[![Deadpool](https://repl.it/badge/github/quiec/whatsasena)](https://replit.com/@confrontermfisa/SUPERBOT-PairCode-4?s=app)
+
+
+# 🐸```Deployment🖤```
+  # 😍Tutorial how to deploy
+[Video Tutorial](https://whatsapp.com/channel/0029Vag3MeuGJP8LZb1Okj39/116)
+## Heroku Buildpack
+```bash
+heroku/nodejs
+https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest
+https://github.com/DuckyTeam/heroku-buildpack-imagemagick
+```
+*Add your Creds.json to Bumblebeesession file
+* Create a new app at [Heroku](heroku.com)
+* Add Build packs
+* Connect your heroku with your github
+* Locate SUPER-BOT
+* Now deploy.
+* Start the Worker
+* Enjoy the Bot.
+```
+```
+## 🦇For Termux/Ssh/Ubuntu
+```bash
+$ pkg upgrade -y && pkg update -y
+$ pkg install git -y
+$ pkg install nodejs -y
+$ pkg install ffmpeg -y
+$ pkg install imagemagick -y
+$ git clone https://github.com/jayden-official/SUPER-BOT
+$ cd SUPER-BOT
+$ npm i 
+$ npm start
+```
+## 💟For VPS
+```bash
+apt install nodejs 
+apt install git 
+apt apt install ffmpeg 
+apt apt install libwebp 
+apt apt install imagrmagick
+apt install bash
+git clone https://github.com/jayden/SUPER-BOT
+cd SUPER-BOT
+npm start
+```
+## ☯️For 24/7 Activation (Termux)
+```
+npm i -g pm2 && pm2 start index.js && pm2 save && pm2 logs
+```
+
+## ☸️Developer
+
+  - [**Follow Instagram**](https://instagram.com/confronter._)
+- [**WhatsApp Me**](https://wa.me/254796283064)
+## Special thanks TO Confronter🤎
+  
---
+++ config.js
@@ -0,0 +1,89 @@
+import {watchFile, unwatchFile} from 'fs'; 
+import chalk from 'chalk';
+import {fileURLToPath} from 'url';
+import fs from 'fs'; 
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
+
+global.botnumber = "254111728536"
+global.confirmcode = ""
+
+global.owner = [
+  ['263717672068', '👑 𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓 𝐂𝐑𝐄𝐀𝐓𝐎𝐑👑', true], 
+  ['263717672068', '🐉Confronter', true],
+  ['263717672068', '💫𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓 💫', true],
+  ['263717672068', '❗️Confronter', true],
+  ['263717672068', '💫 𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓💫', true],
+];
+
+global.suittag = ['254794597254'];
+global.prems = ['254794597254'];
+
+global.packname = '𒁂⃝ᰲĴΔ¥Đ€Ň ƤΔƤƗƗ™ ͟͞';
+global.author = '★𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓★';
+global.wm = '★𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓★';
+global.igfg = '★𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓★';
+global.wait = '*[❗] 𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓 𝐈𝐒 𝐋𝐎𝐀𝐃𝐈𝐍𝐆...*';
+
+global.imagen1 = fs.readFileSync('./Menu2.jpg');
+global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
+global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
+global.imagen4 = fs.readFileSync('./Menu.png');
+global.imagen5 = fs.readFileSync('./src/+18.jpg');
+global.imagen6 = fs.readFileSync('./Menu3.png');
+
+global.mods = [];
+
+//* *******Tiempo***************
+global.d = new Date(new Date + 3600000);
+global.locale = 'es';
+global.dia = d.toLocaleDateString(locale, {weekday: 'long'});
+global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'});
+global.mes = d.toLocaleDateString('es', {month: 'long'});
+global.año = d.toLocaleDateString('es', {year: 'numeric'});
+global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
+//* ****************************
+global.wm2 = `▸ ${dia} ${fecha}\n▸ 𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓`;
+global.gt = '★𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓★';
+global.mysticbot = '★𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓★';
+global.md = 'https://github.com/jayden-official';
+global.mysticbot = 'https://github.com/jayden-official';
+global.waitt = '*[❗] 𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓 𝐈𝐒 𝐋𝐎𝐀𝐃𝐈𝐍𝐆...*';
+global.waittt = '*[❗]𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓 𝐈𝐒 𝐋𝐎𝐀𝐃𝐈𝐍𝐆...*';
+global.waitttt = '*[❗]𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓 𝐈𝐒 𝐋𝐎𝐀𝐃𝐈𝐍𝐆...*';
+global.nomorown = '254794597254';
+global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
+global.cmenut = '❖––––––『';
+global.cmenub = '┊✦ ';
+global.cmenuf = '╰━═┅═━––––––๑\n';
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     ';
+global.dmenut = '*❖─┅──┅〈*';
+global.dmenub = '*┊»*';
+global.dmenub2 = '*┊*';
+global.dmenuf = '*╰┅────────┅✦*';
+global.htjava = '⫹⫺';
+global.htki = '*⭑•̩̩͙⊱•••• ☪*';
+global.htka = '*☪ ••••̩̩͙⊰•⭑*';
+global.comienzo = '• • ◕◕════';
+global.fin = '════◕◕ • •';
+global.botdate = `⫹⫺ Date :  ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}`; // Asia/Jakarta
+global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${moment.tz('America/Los_Angeles').format('HH:mm:ss')}`;// America/Los_Angeles
+global.fgif = {key: {participant: '0@s.whatsapp.net'}, message: {'videoMessage': {'title': wm, 'h': `Hmm`, 'seconds': '999999999', 'gifPlayback': 'true', 'caption': bottime, 'jpegThumbnail': fs.readFileSync('./Menu.png')}}};
+global.multiplier = 99;
+global.flaaa = [
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=',
+];
+//* ************************
+
+const file = fileURLToPath(import.meta.url);
+watchFile(file, () => {
+  unwatchFile(file);
+  console.log(chalk.redBright('Update \'config.js\''));
+  import(`${file}?update=${Date.now()}`);
+});
