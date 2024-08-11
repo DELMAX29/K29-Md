7f150589b447447db68727a09149804834712039
Blessed Dhewa <154527751+DELMAX29@users.noreply.github.com>
2024-08-11 23:34:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('super - Bot\nWhatsApp Bot MD', {
+say('k29 -Md\nWhatsApp Bot MD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot created by jayden tech`, {
+say(`Bot created by k29promax `, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
