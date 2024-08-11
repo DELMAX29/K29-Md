4ad3fcf2cdfcc759b7530cdc35411f88a822b76b
Blessed Dhewa <154527751+DELMAX29@users.noreply.github.com>
2024-08-11 23:37:28 +0200

    K29promax

--- handler.js
+++ handler.js
@@ -7,7 +7,7 @@ import {unwatchFile, watchFile} from 'fs';
 import fs from 'fs';
 import chalk from 'chalk';
 import mddd5 from 'md5';
-import ws from 'ws';
+import ws from 'ws'; 
 
 /**
  * @type {import('@whiskeysockets/baileys')}
